1fb09ab1c9c4fb8472d30e307d07f6d3b92c72a2
Štěpán Vána <stepanvana10@gmail.com>
2022-06-21 22:46:33 +0200

    Loading files

---
+++ utils/locators.js
@@ -0,0 +1,14 @@
+module.exports = {
+    mochaTimeout: 50000,
+    baseUrl: "https://www.phptravels.net/login",
+    email: "//input[@name='email']",
+    emailTxt: "user@phptravels.com",
+    password: "//input[@name='password']",
+    passwordTxt: "demouser",
+    signIn: "btn btn-default btn-lg btn-block effect ladda-button waves-effect",
+    title: "Dashboard - PHPTRAVELS",
+    buttonText: "submit",
+    okButton: "div.sound-perm > div > button",
+    loginUrl: "https://www.phptravels.net/account/dashboard",
+    timeout: 5000,
+};
