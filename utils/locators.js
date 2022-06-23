be3e1beee6554d52d5e115fdf1418b2bc9fb0b1e
Štěpán Vána <stepanvana10@gmail.com>
2022-06-23 14:06:20 +0200

    Loading files

--- utils/locators.js
+++ utils/locators.js
@@ -8,7 +8,6 @@ module.exports = {
     signIn: "btn btn-default btn-lg btn-block effect ladda-button waves-effect",
     title: "Dashboard - PHPTRAVELS",
     buttonText: "submit",
-    okButton: "div.sound-perm > div > button",
     loginUrl: "https://www.phptravels.net/account/dashboard",
     timeout: 5000,
 };
